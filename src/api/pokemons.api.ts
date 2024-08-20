4835eeeb6627264bbc19beea12c54dbe950b3420
IBNnabole <IBNnabole@gmail.com>
2024-08-20 20:30:41 +0300

    CSS + main

--- src/api/pokemons.api.ts
+++ src/api/pokemons.api.ts
@@ -3,7 +3,8 @@ import axios from 'axios';
 const BASE_URL = 'https://pokeapi.co/api/v2';
 
 export const getPokemons = async (page: number) => {
-  const response = await axios.get(`${BASE_URL}/pokemon?limit=20&offset=20`);
+  const offset = (page - 1) * 20;
+  const response = await axios.get(`${BASE_URL}/pokemon?limit=20&offset=${offset}`);
   return response.data;
 };
 
@@ -11,3 +12,4 @@ export const getPokemonById = async (id: string) => {
   const response = await axios.get(`${BASE_URL}/pokemon/${id}`);
   return response.data;
 };
+
